e0912f666a12e728ed00a68e2adf91cd695f144d
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-05-26 12:05:47 +0200

    Adapt to mirror localrepositories cloned from a bitbucket username

--- trydevpi.py
+++ trydevpi.py
@@ -1,28 +1,26 @@
+import os
 from flask import Flask, render_template
 from flask.ext.cache import Cache
 from mercurial import ui, hg
-from mercurial.hgweb.hgwebdir_mod import findrepos
-
-BASE_URL = 'hg+https://hg.tryton.org/'
-BASE_MODULE_URL = 'hg+https://hg.tryton.org/modules/'
 
 app = Flask(__name__)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 app.config.from_envvar('TRYDEVPI_SETTINGS', silent=True)
+USERNAME = app.config.get('BB_USERNAME', 'trytonspain')
+PREFIX = app.config.get('PREFIX', 'trytonspain')
+BASE_URL = 'hg+https://bitbucket.org/%s/' % (USERNAME)
 
 
 def get_urls(branch_filter=None):
     urls = {}
     lui = ui.ui()
-    lui.readconfig(app.config['HG_CONFIG'], trust=True)
-    paths = lui.configitems('paths')
     new_packages = []
     last_series = None
-    for name, path in findrepos(paths):
-        if (name not in ('trytond', 'proteus', 'tryton')
-                and not name.startswith('modules/')):
-            continue
+    root_path = app.config.get('HG_PATH', '.')
+    for dirname in os.listdir(root_path):
+        name = get_module_name(dirname)
         package = get_package(name)
+        path = os.path.join(root_path, dirname)
         repo = hg.repository(lui, path)
         last_major = {}
         max_version = (-1, -1)
@@ -36,10 +34,24 @@ def get_urls(branch_filter=None):
                 max_version = key
             if last_major.get(key, -1) < bug:
                 last_major[key] = bug
-        if max_version == (-1, -1):
+        for branch in repo.branchmap().keys():
+            if (not repo.branchheads(branch)
+                    or (branch_filter and branch != branch_filter)):
+                continue
+            try:
+                major, minor = map(int, branch.split('.'))
+            except ValueError:
+                continue
+            key = (major, minor)
+            if max_version < key:
+                max_version = key
+        if max_version == (-1, -1) and not branch_filter:
             new_packages.append(package)
             continue
-        last_major[max_version[0], max_version[1] + 1] = -1
+        # If default repository we must increase the last tagged version
+        if not branch_filter:
+            max_version = max_version[0], max_version[1] + 1
+        last_major[max_version[0], max_version[1]] = -1
         for (major, minor), bug in last_major.iteritems():
             bug += 1
             version = get_version(major, minor, bug)
@@ -48,7 +60,7 @@ def get_urls(branch_filter=None):
             if (not repo.branchheads(branch)
                     or (branch_filter and branch != branch_filter)):
                 continue
-            url = get_url(package, branch, version)
+            url = get_url(package, branch, version, dirname)
             urls['%s-%s' % (package, version)] = url
     for package in new_packages:
         version = get_version(*last_series)
@@ -58,11 +70,14 @@ def get_urls(branch_filter=None):
     return urls
 
 
+def get_module_name(name):
+    if 'trytond-' in name:
+        name = name[len('trytond-'):]
+    return name
+
+
 def get_package(name):
-    if name in ('trytond', 'proteus', 'tryton'):
-        return name
-    else:
-        return 'trytond_%s' % name[len('modules/'):]
+    return '%s_%s' % (PREFIX, name)
 
 
 def get_version(major, minor, bug):
@@ -83,11 +98,8 @@ def get_branch(major, minor):
         return '%s.%s' % (major, minor)
 
 
-def get_url(name, branch, version):
-    if name.startswith('trytond_'):
-        url = BASE_MODULE_URL + name[len('trytond_'):]
-    else:
-        url = BASE_URL + name
+def get_url(name, branch, version, reponame=None):
+    url = BASE_URL + (reponame if reponame else name)
     return '%(url)s@%(branch)s#egg=%(name)s-%(version)s' % {
         'url': url,
         'name': name,
